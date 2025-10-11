227927dd7a6fcb4283b082c96c4cc8579b84c419
NooBie <noobiebrawltime@gmail.com>
2025-10-11 13:48:17 +0000

    remove emoji (span) and  add symbol of diary

--- src/components/app/header.tsx
+++ src/components/app/header.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { BookOpenText } from 'lucide-react';
 
 const Header = () => {
   return (
     <header className="flex items-center justify-center gap-4 mb-8 p-4 rounded-lg">
       <div className="glass-effect flex items-center justify-center rounded-lg shadow-lg p-4">
         <h1 className="text-5xl md:text-7xl font-bold tracking-tighter animated-gradient-text flex items-center gap-4">
-          <span className="w-12 h-12 flex items-center justify-center text-4xl">📔</span>
+          <BookOpenText className="w-12 h-12" />
           <span>DIGITAL DIARY</span>
         </h1>
       </div>
