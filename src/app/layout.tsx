e9e3f02987a143b1c48f1dea527a832b204bd954
NooBie <noobiebrawltime@gmail.com>
2025-10-12 03:33:32 +0000

    in right side bottom of screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
           <DiaryProvider>
             <LoadingBlur />
             <ThemeChangeNotifier />
-            <div className="fixed bottom-4 left-4 z-50">
+            <div className="fixed bottom-4 right-4 z-50">
                 <ThemeToggle />
             </div>
             {children}
